7b7a17d75b86236264f8e1f4ed6961ff74da656b
megumii <98658943+Megumiiiiii@users.noreply.github.com>
2022-11-13 07:16:10 +0700

    Create qb.sh

---
+++ qb.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+clear
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+echo -e "\033[0;35m"
+echo " ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░                                                                     ";
+echo " ░░░░░████░░░░░░░░░░░░░░░████░░░░░                                                                     ";
+echo " ░░░░███░░░░░░░░░░░░░░░░░░░███░░░░                                                                     ";
+echo " ░░░███░░░░░░░░░░░░░░░░░░░░░███░░░                                                                     ";
+echo " ░░███░░░░░░░░░░░░░░░░░░░░░░░███░░                                                                     ";
+echo " ░███░░░░░░░░░░░░░░░░░░░░░░░░░███░                                                                     ";
+echo " ████░░░░░░░░░░░░░░░░░░░░░░░░░████                                                                     ";
+echo " ████░░░░░░░░░░░░░░░░░░░░░░░░░████                                                                     ";
+echo " ██████░░░░░░░███████░░░░░░░██████                                                                     ";
+echo " █████████████████████████████████                                                                     ";
+echo " █████████████████████████████████                                                                     ";
+echo " ░███████████████████████████████░                                                                     ";
+echo " ░░████░███████████████████░████░░                                                                     ";
+echo " ░░░░░░░███▀███████████▀███░░░░░░░ ██████╗░░█████╗░  ███╗░░██╗░█████╗░██████╗░███████╗                 ";
+echo " ░░░░░░████──▀███████▀──████░░░░░░ ██╔══██╗██╔══██╗  ████╗░██║██╔══██╗██╔══██╗██╔════╝                 ";
+echo " ░░░░░░█████───█████───█████░░░░░░ ██████╦╝██║░░╚═╝  ██╔██╗██║██║░░██║██║░░██║█████╗░░                 ";
+echo " ░░░░░░███████▄█████▄███████░░░░░░ ██╔══██╗██║░░██╗  ██║╚████║██║░░██║██║░░██║██╔══╝░░                 ";
+echo " ░░░░░░░███████████████████░░░░░░░ ██████╦╝╚█████╔╝  ██║░╚███║╚█████╔╝██████╔╝███████╗                 ";
+echo " ░░░░░░░░█████████████████░░░░░░░░ ╚═════╝░░╚════╝░  ╚═╝░░╚══╝░╚════╝░╚═════╝░╚══════╝                 ";
+echo " ░░░░░░░░░███████████████░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░█████████████░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░░███████████░░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░███──▀▀▀──███░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░███─█████─███░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░░███─███─███░░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░░░█████████░░░░░░░░░░░░                                                                     ";
+echo " ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░                                                                     ";
+echo -e "\e[0m"
+
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+
+echo -e '\e[33mProject =\e[37m' Q-Blockchain Testnet
+echo -e '\e[33mCommunity =\e[37m' BeritaCryptoo
+echo -e '\e[33mDiscord =\e[37m' https://discord.gg/beritacryptoonode
+echo -e '\e[33mTelegram Channel =\e[37m' https://t.me/BeritaCryptoo
+echo -e '\e[33mTelegram Group =\e[37m' https://t.me/BeritaaCryptooGroup
+echo -e '\e[33mTwitter =\e[37m' https://twitter.com/BeritaCryptoo
+echo -e "\e[0m"
+
+echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬"
+
+
+
+sleep 2
+
+echo -e "\e[1m\e[32m1. Install docker... \e[0m" && sleep 1
+sudo apt update -y && sudo apt install jq && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin && sudo apt install docker-compose
+
+echo -e "\e[1m\e[32m1. Clone repo... \e[0m" && sleep 1
+git clone https://gitlab.com/q-dev/testnet-public-tools.git
+
+echo -e "\e[1m\e[32m1. Membuat dir... \e[0m" && sleep 1
+mkdir -p /root/testnet-public-tools/testnet-validator/keystore
+
+echo '╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬ </SUDAH> ╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬'
